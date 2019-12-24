3cd0489ae6900040c6f2dd4f40578bbf20596817
Kevin Tang <kvntng@gmail.com>
2019-12-24 02:46:53 -0500

    Testing on Alexa dashboard

--- alexa.py
+++ alexa.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_ask import Ask, statement, question, session
 import json
 
+# Set Alexa endpoint to ngrok.link/voice_dyson
+# Select second option for endpoint:
+# "My development endpoint is a sub-domain of a domain that has a wildcard certificate from a certificate authority."
 app = Flask(__name__)
 ask = Ask(app, '/voice_dyson')
 app.config["ASK_APPLICATION_ID"] = "amzn1.ask.skill.e033e61a-290c-46cf-b4cb-679d9ec858a4"
@@ -24,18 +27,21 @@ def set_fan_settings(temp_mode, fan_num, degrees, power_mode):
     # fan_num = get_slot_value(handler_input=handler_input, slot_name="fan_num")
     # degrees = get_slot_value(handler_input=handler_input, slot_name="degrees")
 
+    output = []
+
     if power_mode:
-        statement(f'Power was toggled to {power_mode}')
+        output.append(f'Power was toggled to {power_mode}')
+    # TODO: Temp mode does not resolve to hot and cold, can be warm or cool currently
     if temp_mode:
-        statement(f'Temp mode was toggled to {temp_mode}')
+        output.append(f'Temp mode was toggled to {temp_mode}')
     if fan_num:
-        statement(f'Fan number was toggled to {fan_num}')
+        output.append(f'Fan number was toggled to {fan_num}')
     if degrees:
-        statement(f'Fan was toggled to {degrees} degrees')
+        output.append(f'Fan was toggled to {degrees} degrees')
 
-    speech_text = "Ending set fan settings intent handling"
+    output.append('Ending set fan settings intent handling')
 
-    return statement(speech_text)
+    return statement(', '.join(output))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
